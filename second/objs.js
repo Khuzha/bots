f7c56162f9dac81c285b278af2352518cef71390
Khuzha <t.sardorkhuja@gmail.com>
2018-08-30 19:45:29 +0500

    I am suspending tryings to create a bot to learn JS.

--- second/objs.js
+++ second/objs.js
@@ -1,12 +1,3 @@
 module.exports = {
-  messageOptions: {
-    parse_mode: "HTML",
-    disable_web_page_preview: false,
-    reply_markup: JSON.stringify({
-        inline_keyboard: [[{
-            text: 'Название кнопки',
-            callback_data: 'do_something'
-        }]]
-    })
-}
+  
 }
