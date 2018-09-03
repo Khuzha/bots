8a9c3ab618dae4814f9d05a39ea726d57d7b205e
Khuzha <t.sardorkhuja@gmail.com>
2018-09-03 20:59:45 +0500

    /

--- artelbot/index.js
+++ artelbot/index.js
@@ -4,33 +4,26 @@ const data = require('./data')
 const dbUrl = 'mongodb://localhost:27017/'
 const bot = new telegraf('619360663:AAG77yeLT3RMRp5B8ra9TR41dh7Lp-Cb_zs')
 
-var rcvdUserData, rcvdDB
+var rcvdUserData, rcvdDB, db
 
-
-mongo.connect(dbUrl, {useNewUrlParser: true}, function(err, client){
-	if(err) return console.log('Ошибка подключения к ДБ на строке: ' + err)
-  db = client.db('navi')
-	const collection = db.collection('lastAction')
-	var dbAdd = (clientID, action) => {
-		db.lastAction.updateOne({id: ClientID}, {$set: {lastAction: action}})
-	}
-	var dbAsk = (clientID) => {
-		rcvdDB = collection.find().toArray(function(err, results){
-			return results
-			client.close
-		})
-		rcvdUserData = collection.find({id: clientID}).toArray(function(err, results){
-			return results
-			client.close
-		})
-	}
+mongo.connect('mongodb://localhost:27017', {useNewUrlParser: true}, (err, client) => {
+    if(err) throw err
+    db = client.db('one')
 })
 
-console.log('DB: \n' + rcvdDB + '\n\n__________________________________________________ \n User: \n' +rcvdUserData)
+bot.on('text', (ctx) => {
+    let id = ctx.chat.id
+    db.collection('list').find({id: id}).toArray()
+        .then((docs) => {
+            console.log(docs);
+        })
+})
+
+//console.log('DB: \n' + rcvdDB + '\n\n__________________________________________________ \n User: \n' +rcvdUserData)
 
 bot.start((ctx) => {
-	(ctx.reply('Начали! Выберите бренд:', {reply_markup: {keyboard: data.brands, resize_keyboard: true}}))
-	db.collection.list.find().toArray()
+	ctx.reply('Начали! Выберите бренд:', {reply_markup: {keyboard: data.brands, resize_keyboard: true}})
+	//ctx.answerCbQuery('Добро пожаловать!')
 })
 
 
@@ -41,16 +34,12 @@ bot.on('text', (ctx) => {
 		//ctx.reply('Вы не выбрали бренд. Пожалуйста, нажмите одну из кнопок ниже:', {reply_markup: {keyboard: data.brands, resize_keyboard: true}})
 	switch (ctx.message.text) {
 		case 'Artel': case 'artel': ctx.reply('Выберите категорию устройства Artel:', {reply_markup: {keyboard: data.categories.Artel, resize_keyboard: true}})
-		dbAdd(ctx.message.from.id, 'Artel')
 		break
 		case 'Royal': case 'royal': ctx.reply('Выберите категорию устройства Royal:', {reply_markup: {keyboard: data.categories.Royal, resize_keyboard: true}})
-		dbAdd(ctx.message.from.id, 'Royal')
 		break
 		case 'Samsung': case 'samsung': ctx.reply('Выберите категорию устройства Samsung:', {reply_markup: {keyboard: data.categories.Samsung, resize_keyboard: true}})
-		dbAdd(ctx.message.from.id, 'Samsung')
 		break
 		case 'Shivaki': case 'shivaki': ctx.reply('Выберите категорию устройства Shivaki:', {reply_markup: {keyboard: data.categories.Shivaki, resize_keyboard: true}})
-		dbAdd(ctx.message.from.id, 'Shivaki')
 		break
 		case '⬅️ Назад': ctx.reply('Вернулись назад. Выберите бренд:', {reply_markup: {keyboard: data.brands, resize_keyboard: true}})
 		break
