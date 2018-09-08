55e139ff43b652d349df513bd245da3708e24166
Sardor <34943401+Khuzha@users.noreply.github.com>
2018-09-08 19:51:52 +0500

    ..

--- contactbot/index.js
+++ contactbot/index.js
@@ -16,3 +16,13 @@ bot.on('text', (ctx) => {
 var hello = 'Здравствуйте! Я — бот-ассистент Сардора, которому Вы собираетесь написать. Оставьте свое сообщение и он свяжется с Вами, как только сможет.'
 
 bot.startPolling()
+
+/* Ошибка: 
+
+Failed to process updates. TypeError: bot.forwardMessage is not a function
+    at bot.on (/home/oneuser/MyProjects/bots/contactbot/index.js:10:7)
+    at Promise.resolve.then (/home/oneuser/MyProjects/bots/contactbot/node_modules/telegraf/composer.js:107:56)
+    at <anonymous>
+    at process._tickCallback (internal/process/next_tick.js:188:7)
+
+*/
