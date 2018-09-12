0bc2dc2c7d95a53fdacd3cc7789047f099cb33a4
Khuzha <t.sardorkhuja@gmail.com>
2018-09-12 23:34:04 +0500

    ...

--- contactbot/index.js
+++ contactbot/index.js
@@ -6,22 +6,24 @@ bot.start((ctx) => {
   if (ctx.chat.id == data.myid){
     ctx.reply(data.startMessageToMe)
   } else {
-  ctx.reply(data.hello, {reply_markup: {inline_keyboard: [[{text: '🇬🇧 Change lang', callback_data: 'en'}]]}})
+  ctx.reply(data.hello/*, {reply_markup: {inline_keyboard: [[{text: '🇬🇧 Change lang', callback_data: 'en'}]]}}*/)
   }
 })
 
 bot.on('text', (ctx) => {
-  if(ctx.chat.id == data.myid && ctx.update.message && ctx.update.message.reply_to_message && ctx.message.reply_to_message.from.is_bot == false){ //чекаю, я ли в чате; ответ ли это на какое-то сообщение; не боту ли я ответил
-    data.lastID = ctx.message.reply_to_message.forward_from.id //присваиваю переменной (можно было записать в виде переменной, но мне так нравится больше) айди того, кому я отвечаю
-    if(data.lastID != ''){ //проверяю, не пустой ли lastID, ибо по умолчанию он именно == '', но пердыдущая строка должна была записать айди
-       ctx.telegram.sendMessage(data.lastID, ctx.message.text)
-     } else {
-       ctx.reply(data.emptyReciever)
-     }
-} else {
-  ctx.forwardMessage(data.myid, ctx.from.id, ctx.message.id)
-}
+  if (ctx.chat.id == data.myid){
+    if(ctx.update.message && ctx.update.message.reply_to_message && ctx.message.reply_to_message.forward_from) {
 
+      if(!ctx.message.reply_to_message.forward_from.is_bot){
+      data.lastID = ctx.message.reply_to_message.forward_from.id//уже понял, что бесполезная строка, но пытался воплотить именно так..
+      ctx.telegram.sendMessage(data.lastID, ctx.message.text)
+    }
+    } else {
+      ctx.reply(data.emptyReciever)
+    }
+  } else {
+    ctx.forwardMessage(data.myid, ctx.from.id, ctx.message.id)
+  }
 })
 
 bot.startPolling()
