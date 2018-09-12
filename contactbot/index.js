430e30a64a03000a8f466cb47629a5fa00735763
Khuzha <t.sardorkhuja@gmail.com>
2018-09-12 22:46:58 +0500

    //

--- contactbot/index.js
+++ contactbot/index.js
@@ -11,16 +11,18 @@ bot.start((ctx) => {
 })
 
 bot.on('text', (ctx) => {
-  if(ctx.chat.id == data.myid && ctx.update.message && ctx.update.message.reply_to_message && ctx.message.reply_to_message.from.is_bot == false){ //чекаю, я ли в чате; ответ ли это на какое-то сообщение; не боту ли я ответил
-    data.lastID = ctx.message.reply_to_message.forward_from.id //присваиваю переменной (можно было записать в виде переменной, но мне так нравится больше) айди того, кому я отвечаю
-    if(data.lastID != ''){ //проверяю, не пустой ли lastID, ибо по умолчанию он именно == '', но пердыдущая строка должна была записать айди
-       ctx.telegram.sendMessage(data.lastID, ctx.message.text)
-     } else {
-       ctx.reply(data.emptyReciever)
-     }
-
+  if(ctx.chat.id == data.myid && ctx.update.message && ctx.update.message.reply_to_message && ctx.update.message && ctx.update.message.reply_to_message){
+    console.log('ok')
+    data.lastID = ctx.message.reply_to_message.forward_from.id
 } else {
   ctx.forwardMessage(data.myid, ctx.from.id, ctx.message.id)
+}
+if(ctx.message.reply_to_message.from.is_bot == false){
+  if(data.lastID != ''){
+     ctx.telegram.sendMessage(data.lastID, ctx.message.text)
+   } else {
+     ctx.reply(data.emptyReciever)
+   }
 }
 })
 
