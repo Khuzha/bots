a8002a32ee15e361bb9f696ef422a989f6216959
Khuzha <t.sardorkhuja@gmail.com>
2018-09-17 00:14:31 +0500

    ...

---
+++ cafebot/index.js
@@ -0,0 +1,25 @@
+const telegraf = require('telegraf')
+const mongo = require('mongodb').MongoClient
+const data = require('./data')
+const bot = new telegraf(data.token)
+var basket
+
+bot.start((ctx) => {
+  ctx.reply(data.answers.welcome, {reply_markup: {keyboard: data.keys.welcome, resize_keyboard: true}})
+})
+
+bot.on('text', (ctx) => {
+  switch(ctx.message.text) {
+    case '📖Меню': ctx.reply(data.answers.menu, {reply_markup: {keyboard: data.keys.menu, resize_keyboard: true}})
+    break
+    case '🛒Корзина': ctx.reply(data.answers.basket, {reply_markup: {keyboard: data.keys.basket, resize_keyboard: true}})
+    break
+    case '📍Все рестораны': ctx.reply(data.answers.restaurants, {reply_markup: {keyboard: data.keys.restaurants, resize_keyboard: true}})
+    break
+  }
+})
+
+bot.catch((err) => {
+  console.log(err)
+})
+bot.startPolling()
