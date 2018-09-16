c671942d7e8eeea7a2deeda022e8626868dad2ba
Khuzha <t.sardorkhuja@gmail.com>
2018-09-17 01:21:00 +0500

    ..

--- cafebot/index.js
+++ cafebot/index.js
@@ -9,13 +9,19 @@ bot.start((ctx) => {
 })
 
 bot.on('text', (ctx) => {
+
   switch(ctx.message.text) {
-    case '📖Меню': ctx.reply(data.answers.menu, {reply_markup: {keyboard: data.keys.menu, resize_keyboard: true}})
+    case '📖Меню': case '⬅️Меню': ctx.reply(data.answers.menu, {reply_markup: {keyboard: data.keys.menu, resize_keyboard: true}})
     break
     case '🛒Корзина': ctx.reply(data.answers.basket, {reply_markup: {keyboard: data.keys.basket, resize_keyboard: true}})
     break
     case '📍Все рестораны': ctx.reply(data.answers.restaurants, {reply_markup: {keyboard: data.keys.restaurants, resize_keyboard: true}})
     break
+    case '⬅️Главное меню': ctx.reply(data.answers.welcomeBack, {reply_markup: {keyboard: data.keys.welcome, resize_keyboard: true}})
+    break
+    case '🍔Сэндвичи': ctx.reply(data.answers.sandwiches, {reply_markup: {keyboard: data.keys.sandwiches, resize_keyboard: true}})
+    break
+    case 'Ай-Твистер Чиз': ctx.replyWithPhoto(data.other['Ай-Твистер Чиз'].url, {caption: data.other['Ай-Твистер Чиз'].caption, parse_mode: 'markdown'}, {reply_markup: {inline_keyboard: data.inlineKeys.twister}})
   }
 })
 
